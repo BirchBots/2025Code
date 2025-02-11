599e29fbd3ada0886c7bf6910bddbfa0d00c0951
Gray720 <grayson.green2028@gbwl.org>
2025-02-11 16:13:48 -0500

    started toggle method

--- src/main/java/frc/robot/Subsystems/ClimbSubSystem.java
+++ src/main/java/frc/robot/Subsystems/ClimbSubSystem.java
@@ -1,13 +1,18 @@
+package frc.robot.Subsystems;
+
 import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.RelativeEncoder;
 import com.revrobotics.spark.SparkMax;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ClimbSubSystem extends SubsystemBase{
     private final static SparkMax ClimbMotor = new SparkMax(0,MotorType.kBrushless);
-        public static void up() {
-            ClimbMotor.set(0.8);
-    } public static void down() {
-        ClimbMotor.set(-0.8);
+    private final RelativeEncoder climbEncoder = ClimbMotor.getEncoder();
+    private final float upRad = (float)Math.PI/2;
+    private final float downrad = (float)Math.PI;
+    public void toggle() {
+        if ()
     }
+    
 }
