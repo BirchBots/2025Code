abbc555b6d4bced97ad3aa405c19150a929c51c6
Gray720 <grayson.green2028@gbwl.org>
2025-02-12 16:15:07 -0500

    yaya

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,14 +1,21 @@
 package frc.robot;
 
+
+
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Subsystems.ClimbSubSystem;
 
 public class RobotContainer {
-
+private final static XboxController Controller = new XboxController(0);
+private final static ClimbSubSystem Climb = new ClimbSubSystem();
   public RobotContainer() {
     configureBindings();
   }
 
-  private void configureBindings() {}
+  private void configureBindings() {
+
+  }
 
   public Command getAutonomousCommand() {
     return null;
--- src/main/java/frc/robot/Subsystems/ClimbSubSystem.java
+++ src/main/java/frc/robot/Subsystems/ClimbSubSystem.java
@@ -9,10 +9,18 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class ClimbSubSystem extends SubsystemBase{
     private final static SparkMax ClimbMotor = new SparkMax(0,MotorType.kBrushless);
     private final RelativeEncoder climbEncoder = ClimbMotor.getEncoder();
-    private final float upRad = (float)Math.PI/2;
-    private final float downrad = (float)Math.PI;
+    private final double upRad = Math.PI/2; 
+    private final double speed = 0.4;
+    private final double downRad = Math.PI;
     public void toggle() {
-        if ()
+        if (climbEncoder.getPosition() == upRad){ //if up is true write it here
+            ClimbMotor.set(speed);
+
+        } else if (climbEncoder.getPosition() == downRad) {
+            ClimbMotor.set(-speed);
+        
+
+        }
     }
     
 }
