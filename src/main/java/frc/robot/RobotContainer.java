91e68e13e908eb4624bfda68d877cfb1d5e41a49
caleb <caleb.fisher2027@gbwl.org>
2025-02-13 16:13:26 -0500

    created auto chooser

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,16 +1,24 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 
 public class RobotContainer {
-
+  SendableChooser<Command> m_chooser = new SendableChooser<Command>();
   public RobotContainer() {
+    m_chooser.setDefaultOption("Middle", new InstantCommand());
+    m_chooser.addOption("right", new InstantCommand());
+    m_chooser.addOption("left", new InstantCommand());
+    SmartDashboard.putData(m_chooser);
     configureBindings();
   }
 
   private void configureBindings() {}
+    
 
   public Command getAutonomousCommand() {
-    return null;
+    return m_chooser.getSelected();
   }
 }
