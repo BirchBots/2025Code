305be53fdbdec6351bea87071f4491a1bd3a78bc
KaiMeiLi <103324651+KaiMeiLi@users.noreply.github.com>
2025-03-04 15:18:51 -0500

    jdighsug

--- src/main/java/frc/robot/Subsystems/Coral.java
+++ src/main/java/frc/robot/Subsystems/Coral.java
@@ -8,19 +8,19 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class Coral extends SubsystemBase{
 
     private final SparkMax downMotor = new SparkMax(0, MotorType.kBrushless);
-    private final SparkMax upMotor = new SparkMax(0, MotorType.kBrushless);
+    
     private final SparkMax throwCoralMotor = new SparkMax(0, MotorType.kBrushless);
-    private final SparkMax pickCoralMotor = new SparkMax(0, MotorType.kBrushless);
+    
  private final double speed = 0.5;
  private final double rSpeed = 0.1;
 
- //Move coral, more effecient way to do?
+ //Move coral, more effecient way to
  public void setDownSpeed() { 
    downMotor.set(-rSpeed);
  }
 
  public void setUpSpeed() {
-    upMotor.set(rSpeed);
+    downMotor.set(rSpeed);
  }
 
 
@@ -29,7 +29,7 @@ public class Coral extends SubsystemBase{
     throwCoralMotor.set(speed);
  }
  public void setPickCoral() {
-    pickCoralMotor.set(-speed);
+    throwCoralMotor.set(-speed);
  }
 
 
